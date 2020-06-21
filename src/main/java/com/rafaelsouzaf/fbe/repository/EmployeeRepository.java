28c341faef22479884b5ebf759a9777bc74bd028
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2020-06-21 18:58:16 +0200

    Add custom Query for calculate the average salary

--- src/main/java/com/rafaelsouzaf/fbe/repository/EmployeeRepository.java
+++ src/main/java/com/rafaelsouzaf/fbe/repository/EmployeeRepository.java
@@ -2,7 +2,14 @@ package com.rafaelsouzaf.fbe.repository;
 
 import com.rafaelsouzaf.fbe.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT AVG(e.salary) FROM Employee e WHERE e.company.id=:companyId")
+    BigDecimal getAverageSalary(@Param("companyId") Long companyId);
 
 }
